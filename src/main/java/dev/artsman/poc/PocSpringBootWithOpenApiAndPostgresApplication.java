e30ff3fd174535fc884da0a962edb396dd968030
Bruno Andrade <bruno@artsman.dev>
2024-03-30 18:34:44 +0000

    (feat): added main bootstrap

---
+++ src/main/java/dev/artsman/poc/PocSpringBootWithOpenApiAndPostgresApplication.java
@@ -0,0 +1,11 @@
+package dev.artsman.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class PocSpringBootWithOpenApiAndPostgresApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(PocSpringBootWithOpenApiAndPostgresApplication.class, args);
+	}
+}
